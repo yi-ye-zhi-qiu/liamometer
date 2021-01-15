ecb6ae842439c613723cc15b34c2b16fa6af7b7b
Liam Isaacs <yeqiu194@gmail.com>
2021-01-15 12:33:55 -0800

    clean-up mojo spider

--- boxoffice_scrapy/boxoffice_scrapy/spiders/spider.py
+++ boxoffice_scrapy/boxoffice_scrapy/spiders/spider.py
@@ -13,10 +13,8 @@ class mojo_spider(scrapy.Spider):
     ]
     #note: 2017, 2018, 2019 have ~800  rows
     #note: 2020 is limiting @ 454
-
     for year in [2017, 2018, 2019, 2020]:
         start_urls.append("https://www.boxofficemojo.com/year/"+str(year)+"/")
-
     def parse(self, response):
         #for TESTING
         #for tr in response.xpath('//*[@id="table"]/div/table/tr')[1:2]:
@@ -28,52 +26,12 @@ class mojo_spider(scrapy.Spider):
                 yield scrapy.Request(url, callback=self.parse_page_contents)
             except IndexError as ie:
                 print("Ignoring error in '{}': '{}'.".format(url, ie))
-
     def parse_page_contents(self, response):
         item = BoxItem()
 
         elements = []
         for div in response.xpath('//*[@id="a-page"]/main/div/div[3]/div[4]/div')[0:]:
             elements.append(' '.join(div.xpath('./span[1]/text()')[0].extract().split()))
-            #dictionaries idea?
-            # wanted_elms = ['MPAA', 'Distributor', 'Budget', 'Genres', 'Release Date', 'Opening', 'Widest Release']
-            #
-            # def enter_into_dict(i, dict):
-            #     for v in dict:
-            #         if i ==v and i in elements:
-            #             m = elements.index(i) +1
-            #             dict[v] == response.xpath('//*[@id="a-page"]/main/div/div[3]/div[4]/div[{}]/span[2]/text()'.format(m))[0].extract()
-            #         else:
-            #             dict[v] = 'N/A'
-            #
-            # for i in wanted_elms:
-            #     #we iterate through MPAA, Budget etc.
-            #     #and say ok run get_page
-            #     #get_page will update the dictionary's entry
-            #     #ONLY for that key
-            #     #initialize dict
-            #     elements_ = {'MPAA': '',
-            #                  'Budget': '',
-            #                  'Release Date': '',
-            #                  'Genres': '',
-            #                  'Opening': '',
-            #                  'Widest Release': '',
-            #                  'Distributor': ''}
-            #     #now edit
-            #     elements_ = {'MPAA': ' '.join(enter_into_dict(i, elements_).split()),
-            #                  'Budget': enter_into_dict(i ,elements_),
-            #                  'Release Date': enter_into_dict(i, elements_),
-            #                  'Genres': ', '.join(enter_into_dict(i, elements_).split()),
-            #                  'Opening': enter_into_dict(i, elements_),
-            #                  'Widest Release': enter_into_dict(i, elements_),
-            #                  'Distributor': enter_into_dict(i, elements_)}
-            #     elements__ = {x.replace(' ', '_'): v for x, v in elements_.items()}
-            #     #if element is None (as in no Budget, no MPAA, etc.), return "N/A" instead
-            #     for k, v in elements__.items():
-            #         if v is None:
-            #             elements__[k] = "N/A"
-            #     item[i] = elements__[i]
-
 
             if 'MPAA' in elements:
                 m = elements.index('MPAA') + 1
@@ -118,113 +76,100 @@ class mojo_spider(scrapy.Spider):
             else:
                 item['distributor'] = 'N/A'
 
-            # else:
-            #     item["MPAA"] = "N/A"
-            #     item["budget"] = "N/A"
-            #     item["release_days"] = "N/A"
-            #     item["genres"] = "N/A"
-            #     item["opening_revenue"] = "N/A"
-            #     item["opening_theaters"] = "N/A"
-            #     item["distributor"] = "N/A"
-
         item['title'] = response.xpath('//*[@id="a-page"]/main/div/div[1]/div[1]/div/div/div[2]/h1/text()')[0].extract()
         item['domestic_revenue'] = response.xpath('//*[@id="a-page"]/main/div/div[3]/div[1]/div/div[1]/span[2]/span/text()')[0].extract()
         item['world_revenue'] = response.xpath('//*[@id="a-page"]/main/div/div[3]/div[1]/div/div[3]/span[2]/a/span/text()')[0].extract()
-        #item['distributor'] = response.xpath('//*[@id="a-page"]/main/div/div[3]/div[4]/div[1]/span[2]/text()')[0].extract()
-        #item['opening_revenue'] = response.xpath('//*[@id="a-page"]/main/div/div[3]/div[4]/div[2]/span[2]/span/text()')[0].extract()
-        #item['opening_theaters'] = ' '.join(response.xpath('//*[@id="a-page"]/main/div/div[3]/div[4]/div[2]/span[2]/text()')[0].extract().split())
-        #item['genres'] = ','.join(response.xpath('//*[@id="a-page"]/main/div/div[3]/div[4]/div[7]/span[2]/text()')[0].extract().split())
         yield item
 
-# import scrapy
-# from scrapy import Spider, Selector
-# import random
-# import requests
-# import re
-# import math
-# import time
-# import colors
-# from termcolor import colored
-#
-#
-# class tomato_spider(scrapy.Spider):
-#     name = 'tomato_spider'
-#     allowed_domains = ['https://www.rottentomatoes.com']
-#     start_urls = ["https://www.rottentomatoes.com/top/bestofrt/top_100_action__adventure_movies/",
-#                   "https://www.rottentomatoes.com/top/bestofrt/top_100_animation_movies/",
-#                   "https://www.rottentomatoes.com/top/bestofrt/top_100_art_house__international_movies/",
-#                   "https://www.rottentomatoes.com/top/bestofrt/top_100_classics_movies/",
-#                   "https://www.rottentomatoes.com/top/bestofrt/top_100_comedy_movies/",
-#                   "https://www.rottentomatoes.com/top/bestofrt/top_100_documentary_movies/",
-#                   "https://www.rottentomatoes.com/top/bestofrt/top_100_drama_movies/",
-#                   "https://www.rottentomatoes.com/top/bestofrt/top_100_horror_movies/",
-#                   "https://www.rottentomatoes.com/top/bestofrt/top_100_kids__family_movies/",
-#                   "https://www.rottentomatoes.com/top/bestofrt/top_100_musical__performing_arts_movies/",
-#                   "https://www.rottentomatoes.com/top/bestofrt/top_100_mystery__suspense_movies/",
-#                   "https://www.rottentomatoes.com/top/bestofrt/top_100_romance_movies/",
-#                   "https://www.rottentomatoes.com/top/bestofrt/top_100_science_fiction__fantasy_movies/",
-#                   "https://www.rottentomatoes.com/top/bestofrt/top_100_special_interest_movies/",
-#                   "https://www.rottentomatoes.com/top/bestofrt/top_100_sports__fitness_movies/",
-#                   "https://www.rottentomatoes.com/top/bestofrt/top_100_television_movies/",
-#                   "https://www.rottentomatoes.com/top/bestofrt/top_100_western_movies/"]
-#
-#     soft_failures = 0
-#     hard_failures = 0
-#
-#     def parse(self, response):
-#         top100 = response.xpath('/html/body/div[4]/div[2]/div[1]/section/div/table//@href').extract()
-#
-#         for selector in top100:
-#             url = 'https://www.rottentomatoes.com' + selector
-#             try:
-#                 yield self.get_movieinfo(url)
-#             except IndexError as ie:
-#                 print("Ignoring error in '{}': '{}'.".format(url, ie))
-#
-#     @staticmethod
-#     def _get_content(link):
-#         """
-#         Politely request the page content at link.
-#         """
-#
-#         content = requests.get(link).text
-#
-#         x = 3 + 2 * random.random()
-#         time.sleep(x)
-#
-#         return content
-#
-#     def get_movieinfo(self, url):
-#         print("Getting movie info for '{}'".format(url))
-#
-#         content = tomato_spider._get_content(url)
-#
-#         def make_xpath(sibling_value):
-#             return '//div[@class="meta-label subtle" and text()="{}: "]/following-sibling::div/text()'.format(sibling_value)
-#
-#         def grab_data(name, my_xpath, force=False):
-#             def print_failures(is_hard):
-#                 print("\n{} fail to xpath '{}'!! '{}'. \nsoft fails: {}, hard_fails: {}\n".format(
-#                     "\tHARD" if is_hard else "Soft", name, my_xpath, self.soft_failures, self.hard_failures))
-#
-#             try:
-#                 return Selector(text=content).xpath(my_xpath)[0].extract().strip()
-#             except Exception as e:
-#                 if force:
-#                     self.soft_failures += 1
-#                     print_failures(False)
-#                     return None
-#
-#                 self.hard_failures += 1
-#                 print_failures(True)
-#                 raise e
-#
-#         return {
-#             "title": grab_data("title", '//h1[@class="mop-ratings-wrap__title mop-ratings-wrap__title--top"]/text()'),
-#             "criticscore": grab_data("criticscore", '//*[@id="tomato_meter_link"]/span[2]/text()'),
-#             "criticcount": grab_data("critic count", '//small[@class="mop-ratings-wrap__text--small"]/text()'),
-#             "audiencescore": grab_data("audiencescore", '//*[@id="topSection"]/div[2]/div[1]/section/section/div[2]/h2/a/span[2]/text()')
-#         }
+import scrapy
+from scrapy import Spider, Selector
+import random
+import requests
+import re
+import math
+import time
+import colors
+from termcolor import colored
+
+
+class tomato_spider(scrapy.Spider):
+    name = 'tomato_spider'
+    allowed_domains = ['https://www.rottentomatoes.com']
+    start_urls = ["https://www.rottentomatoes.com/top/bestofrt/top_100_action__adventure_movies/",
+                  "https://www.rottentomatoes.com/top/bestofrt/top_100_animation_movies/",
+                  "https://www.rottentomatoes.com/top/bestofrt/top_100_art_house__international_movies/",
+                  "https://www.rottentomatoes.com/top/bestofrt/top_100_classics_movies/",
+                  "https://www.rottentomatoes.com/top/bestofrt/top_100_comedy_movies/",
+                  "https://www.rottentomatoes.com/top/bestofrt/top_100_documentary_movies/",
+                  "https://www.rottentomatoes.com/top/bestofrt/top_100_drama_movies/",
+                  "https://www.rottentomatoes.com/top/bestofrt/top_100_horror_movies/",
+                  "https://www.rottentomatoes.com/top/bestofrt/top_100_kids__family_movies/",
+                  "https://www.rottentomatoes.com/top/bestofrt/top_100_musical__performing_arts_movies/",
+                  "https://www.rottentomatoes.com/top/bestofrt/top_100_mystery__suspense_movies/",
+                  "https://www.rottentomatoes.com/top/bestofrt/top_100_romance_movies/",
+                  "https://www.rottentomatoes.com/top/bestofrt/top_100_science_fiction__fantasy_movies/",
+                  "https://www.rottentomatoes.com/top/bestofrt/top_100_special_interest_movies/",
+                  "https://www.rottentomatoes.com/top/bestofrt/top_100_sports__fitness_movies/",
+                  "https://www.rottentomatoes.com/top/bestofrt/top_100_television_movies/",
+                  "https://www.rottentomatoes.com/top/bestofrt/top_100_western_movies/"]
+
+    soft_failures = 0
+    hard_failures = 0
+
+    def parse(self, response):
+        top100 = response.xpath('/html/body/div[4]/div[2]/div[1]/section/div/table//@href').extract()
+
+        for selector in top100:
+            url = 'https://www.rottentomatoes.com' + selector
+            try:
+                yield self.get_movieinfo(url)
+            except IndexError as ie:
+                print("Ignoring error in '{}': '{}'.".format(url, ie))
+
+    @staticmethod
+    def _get_content(link):
+        """
+        Politely request the page content at link.
+        """
+
+        content = requests.get(link).text
+
+        x = 3 + 2 * random.random()
+        time.sleep(x)
+
+        return content
+
+    def get_movieinfo(self, url):
+        print("Getting movie info for '{}'".format(url))
+
+        content = tomato_spider._get_content(url)
+
+        def make_xpath(sibling_value):
+            return '//div[@class="meta-label subtle" and text()="{}: "]/following-sibling::div/text()'.format(sibling_value)
+
+        def grab_data(name, my_xpath, force=False):
+            def print_failures(is_hard):
+                print("\n{} fail to xpath '{}'!! '{}'. \nsoft fails: {}, hard_fails: {}\n".format(
+                    "\tHARD" if is_hard else "Soft", name, my_xpath, self.soft_failures, self.hard_failures))
+
+            try:
+                return Selector(text=content).xpath(my_xpath)[0].extract().strip()
+            except Exception as e:
+                if force:
+                    self.soft_failures += 1
+                    print_failures(False)
+                    return None
+
+                self.hard_failures += 1
+                print_failures(True)
+                raise e
+
+        return {
+            "title": grab_data("title", '//h1[@class="mop-ratings-wrap__title mop-ratings-wrap__title--top"]/text()'),
+            "criticscore": grab_data("criticscore", '//*[@id="tomato_meter_link"]/span[2]/text()'),
+            "criticcount": grab_data("critic count", '//small[@class="mop-ratings-wrap__text--small"]/text()'),
+            "audiencescore": grab_data("audiencescore", '//*[@id="topSection"]/div[2]/div[1]/section/section/div[2]/h2/a/span[2]/text()')
+        }
 
 # # ------------ TESTING OUR SCRAPY: ------------
 #
