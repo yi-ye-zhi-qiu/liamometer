1ff3fdc934d1dad1542edc3e949f240964450e45
yi-ye-zhi-qiu <yeqiu194@gmail.com>
2021-01-19 13:40:41 -0800

    ++dogs example

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-/*
-Configure as webapp
-*/
-const puppeteer = require('puppeteer');
+fetch("https://dog.ceo/api/breeds/image/random/10")
+  .then((response) => response.json())
+  .then((data) => data.message)
+  .then((arrayOfLinks) => addPhoto(arrayOfLinks))
+  .catch((e) => console.log(e));
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('')
-  const price = await page.$eval('.', div => div.textContent);
-  console.log(price);
-  await page.waitFor(1000);
-  await browser.close();
-})
+function addPhoto(links) {
+  let gallery= document.querySelector(".gallery");
+
+  for (let link of links) {
+    let image_div = document.createElement("div");
+    let image = document.createElement("img");
+    image.src = link;
+    image.classList.add("img-full")
+    image_div.appendChild(image);
+    image_div.classList.add("img-banner")
+    let rating = document.createElement("div")
+    let rating_text = document.createElement("p")
+    rating_text.classList.add("topleft")
+    rating_text.innerHTML = "40"
+    rating.appendChild(rating_text)
+    image_div.appendChild(rating)
+    gallery.appendChild(image_div);
+  }
+}
