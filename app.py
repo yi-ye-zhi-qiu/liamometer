30bf48bc234e538a5005ee6513fda35b52119669
Liam Isaacs <yeqiu194@gmail.com>
2021-03-31 11:43:29 -0700

    revise app.py to use new functions

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import *
 from flask import Flask, render_template, request, jsonify, make_response
 
 #import models
-from models.liamometer import get_movie_data
+from models.liamometer import _movie_data
 
 app = Flask(__name__)
 
@@ -14,9 +14,9 @@ def show_liamometer():
     Performs linear regression on scraped databases (IMDB, BoxOfficeMojo).
     Returns 500 rows, just because I do not want to overload the page (currently working on lazy-load in javascript, or loading as you scroll.)
     """
-    max, liams_favorite, liams_favorite_movie_image, html_data = get_movie_data()
+    max, liams_favorite, html_data = _movie_data()
 
-    return render_template('liamometer.html', max=max, liams_favorite_movie_image = liams_favorite_movie_image,
+    return render_template('liamometer.html', max=max, liams_favorite_movie_image = '',
                             liams_favorite=liams_favorite, html_data = html_data)
 
 if __name__ == '__main__':
